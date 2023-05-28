ffba77c8190ce564c960346c418596946c492bf7
jhony2488 <jhon.araujo2488@gmail.com>
2023-05-28 17:52:12 -0300

    criado exemplo .env i gitignore

---
+++ babel.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@config': './src/config',
+          '@middlewares': './src/app/middlewares',
+          '@models': './src/app/models',
+          '@controllers': './src/app/controllers',
+          '@views': './src/app/views',
+          '@database': './src/database',
+          '@migrations': './src/database/migrations',
+          '@seeders': './src/database/seeders',
+        },
+      },
+    ],
+  ],
+  ignore: ['**/*.spec.ts'],
+}
