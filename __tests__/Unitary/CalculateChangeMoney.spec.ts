a3c1ee8c9f24bf2c71b03c6f31e30f10fee895b2
jhony2488 <jhon.araujo2488@gmail.com>
2023-05-28 17:49:31 -0300

    configurado jest e desenvolvido testes inicialmente

---
+++ __tests__/Unitary/CalculateChangeMoney.spec.ts
@@ -0,0 +1,10 @@
+import {calculateChangeMoney} from '../../src/app/utils/calculateChangeMoney';
+
+describe('CalculateChangeMoney', () => {
+  it('should return the amount of change and verify that the amount of money paid is less than the amount he must pay', async () => {
+    expect(calculateChangeMoney(10,30)).toStrictEqual({"changeValue": 20, "message": "Não falta dinheiro"});
+  });
+  it('should return the amount of change and verify that the amount of money paid is less than the amount he must pay', async () => {
+    expect(calculateChangeMoney(40,30)).toStrictEqual({"changeValue": -10, "message": "Falta dinheiro"});
+  });
+})
